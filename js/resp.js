8a8936e3de08f7b3814c36b4c33a38e1781d055f
DIKSHA SAINI <69575764+dikshasa17@users.noreply.github.com>
2021-07-06 20:24:02 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -8,3 +8,12 @@ donate.addEventListener('click', () => {
     navList.classList.toggle('v-class-resp')
     navbar.classList.toggle('h-nav-resp')
 })
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
